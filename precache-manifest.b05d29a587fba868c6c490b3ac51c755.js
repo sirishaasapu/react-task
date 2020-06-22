2b01c63e4fe41b934f785cb4f7ef8adaf905335f
durgarao <asapusirisha@gmail.com>
2020-06-22 10:05:27 +0530

    Updates

---
+++ precache-manifest.b05d29a587fba868c6c490b3ac51c755.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f63d2dc883ae2dc6c39c",
+    "url": "/react-task/static/css/main.19a56591.chunk.css"
+  },
+  {
+    "revision": "f63d2dc883ae2dc6c39c",
+    "url": "/react-task/static/js/main.a10f61a3.chunk.js"
+  },
+  {
+    "revision": "42dfcde86f1fb43b7697",
+    "url": "/react-task/static/js/runtime~main.593f8690.js"
+  },
+  {
+    "revision": "eade081ccac8f1d8f51b",
+    "url": "/react-task/static/js/2.99bb1d12.chunk.js"
+  },
+  {
+    "revision": "546358cf7ba497f3c68df3466b0eb2fa",
+    "url": "/react-task/index.html"
+  }
+];
